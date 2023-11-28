b6c6678e8dfb98abc717eff2706c16eb150972ce
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-11-29 00:02:12 +0100

    Correctly mark varbit

--- src/main/java/io/cbitler/stealingartefacts/Constants.java
+++ src/main/java/io/cbitler/stealingartefacts/Constants.java
@@ -1,9 +1,11 @@
 package io.cbitler.stealingartefacts;
 
+import net.runelite.api.annotations.Varbit;
+
 public class Constants {
     final static int PATROL_ID_MIN = 6973;
     final static int PATROL_ID_MAX = 6980;
-    final static int STEALING_ARTEFACTS_VARBIT = 4903;
+    final static @Varbit int STEALING_ARTEFACTS_VARBIT = 4903;
     final static int SOUTH = 0;
     final static int WEST = 512;
 }
