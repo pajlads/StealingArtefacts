a51a3afe6d299e4cb6cda9920a5922eb7e63e80e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-03-16 15:01:25 +0100

    reformat/fix copy paste issues

--- src/main/java/io/cbitler/stealingartefacts/StealingArtefactsKhaledOverlay.java
+++ src/main/java/io/cbitler/stealingartefacts/StealingArtefactsKhaledOverlay.java
@@ -1,10 +1,7 @@
 package io.cbitler.stealingartefacts;
 
 import net.runelite.api.Client;
-import net.runelite.api.NPC;
-import net.runelite.api.Perspective;
 import net.runelite.api.Point;
-import net.runelite.api.coords.LocalPoint;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
@@ -15,11 +12,11 @@ import javax.inject.Inject;
 import java.awt.*;
 
 /**
- * Overlay to highlight the patrol-people in port pisc
+ * Overlay to highlight Captain Khaled
  */
 public class StealingArtefactsKhaledOverlay extends Overlay {
     private static final Color MISSING_TASK_BORDER = Color.YELLOW;
-    private static final Color MISSING_TASK_BORDER_HOVER = Color.GREEN;
+    private static final Color MISSING_TASK_BORDER_HOVER = Color.ORANGE;
     private static final Color MISSING_TASK_FILL = new Color(255, 0, 0, 50);
 
     private final StealingArtefactsPlugin plugin;
@@ -35,42 +32,9 @@ public class StealingArtefactsKhaledOverlay extends Overlay {
         this.config = config;
     }
 
-    @Nullable
-    private Color fillColor() {
-        switch (plugin.currentState) {
-            case NO_TASK:
-            case FAILURE:
-            case DELIVER_ARTEFACT:
-                return MISSING_TASK_FILL;
-
-            default:
-                return null;
-        }
-    }
-
-    @Nullable
-    private Color borderColor() {
-        switch (plugin.currentState) {
-            case NO_TASK:
-            case FAILURE:
-            case DELIVER_ARTEFACT:
-                return MISSING_TASK_BORDER;
-
-            default:
-                return null;
-        }
-    }
-
-    /**
-     * Overlay the patrol-people on the same plane as the player
-     *
-     * @param graphics The graphics to draw the overlay with
-     * @return null, use OverlayUtil to draw overlay
-     */
     @Override
     public Dimension render(Graphics2D graphics) {
         var fillColor = this.fillColor();
-        var borderColor = this.borderColor();
         if (fillColor == null) {
             return null;
         }
@@ -85,9 +49,21 @@ public class StealingArtefactsKhaledOverlay extends Overlay {
 
         Point mousePosition = client.getMouseCanvasPosition();
 
-        OverlayUtil.renderHoverableArea(graphics, plugin.captainKhaled.getConvexHull(),
-                mousePosition, fillColor, MISSING_TASK_BORDER, MISSING_TASK_BORDER_HOVER);
+        OverlayUtil.renderHoverableArea(graphics, plugin.captainKhaled.getConvexHull(), mousePosition, fillColor, MISSING_TASK_BORDER, MISSING_TASK_BORDER_HOVER);
 
         return null;
+    }
+
+    @Nullable
+    private Color fillColor() {
+        switch (plugin.currentState) {
+            case NO_TASK:
+            case FAILURE:
+            case DELIVER_ARTEFACT:
+                return MISSING_TASK_FILL;
+
+            default:
+                return null;
+        }
     }
 }
